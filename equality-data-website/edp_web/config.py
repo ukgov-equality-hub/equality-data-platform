593a8c914bd897b05dd211d57bd3080cbfacf6a8
James Griffiths <james@jwgsoftware.com>
2023-09-25 11:05:32 +0100

    EHD-747: Migrate Gov.UK PaaS to AWS: Use secure cookie (now that the domain has been delegated to our AWS account)
    This reverts commit b6b11008405441e9e2abf5656e0b0cb4b0e0860d.

--- equality-data-website/edp_web/config.py
+++ equality-data-website/edp_web/config.py
@@ -12,7 +12,7 @@ class Config:
         else logging.INFO
     )
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SESSION_COOKIE_SECURE = False
+    SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = "Lax"
     WTF_CSRF_ENABLED = True
